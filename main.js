caefd94e2ba5145d5aeb0f67549cc34861e7b27c
Jared Hanson <jaredhanson@gmail.com>
2013-06-15 09:11:26 -0700

    Implement setupReceiver function.

--- main.js
+++ main.js
@@ -14,14 +14,51 @@
  *  - [rpc.js](http://opensocial-resources.googlecode.com/svn/spec/0.8/gadgets/rpc.js)
  */
 define(['exports',
+        'gadgets-util',
         './lib/transports'],
-function(exports, transports) {
+function(exports, util, transports) {
 
-  var authToken = {};
+  var relayUrl = {}
+    , useLegacyProtocol = {}
+    , authToken = {};
 
   exports.register = function(serviceName, handler) {
     console.log('gadgets.rpc.register');
     console.log('  serviceName: ' + serviceName);
+  }
+  
+  exports.setRelayUrl = function(targetId, url, useLegacy) {
+    console.log('gadgets.rpc.setRelayUrl');
+    console.log('  targetId: ' + targetId);
+    
+    if (!/http(s)?:\/\/.+/.test(url)) {
+      if (url.indexOf("//") == 0) {
+        url = window.location.protocol + url;
+      } else if (url.charAt(0) == '/') {
+        url = window.location.protocol + "//" + window.location.host + url;
+      } else if (url.indexOf("://") == -1) {
+        // Assumed to be schemaless. Default to current protocol.
+        url = window.location.protocol + "//" + url;
+      }
+    }
+    relayUrl[targetId] = url;
+    useLegacyProtocol[targetId] = !!useLegacy;
+  }
+  
+  exports.setAuthToken = function(targetId, token, forceSecure) {
+    console.log('gadgets.rpc.setAuthToken');
+    console.log('  targetId: ' + targetId);
+    console.log('  token: ' + token);
+    
+    
+    token = token || '';
+    authToken[targetId] = String(token);
+    // TODO:
+    //setupFrame(targetId, token, forcesecure);
+  }
+  
+  exports.setupReceiver = function(targetId, url, authToken, forceSecure) {
+    setupChildIframe(targetId, url, authToken, forceSecure)
   }
   
   exports.getAuthToken = function(targetId) {
@@ -30,6 +67,25 @@ function(exports, transports) {
     
     return authToken[targetId];
   }
+  
+  function setupChildIframe(targetId, url, authToken, forceSecure) {
+    var iframe = document.getElementById(targetId);
+    if (!iframe) {
+      throw new Error("Cannot set up gadgets.rpc receiver with ID: " + targetId + ". Element not found.");
+    }
+    
+    // The "relay URL" can be explicitly specified, defaulting to the IFRAME
+    // source.
+    var relayUrl = url || iframe.src;
+    exports.setRelayUrl(targetId, relayUrl);
+
+    // The auth token is parsed from URL params (rpctoken) or overridden.
+    var params = util.getUrlParameters(iframe.src);
+    var rpctoken = authToken || params.rpctoken;
+    var forcesecure = forceSecure || params.forcesecure;
+    exports.setAuthToken(targetId, rpctoken, forcesecure);
+  }
+  
   
   exports.getOrigin = function(url) {
     if (!url) {
